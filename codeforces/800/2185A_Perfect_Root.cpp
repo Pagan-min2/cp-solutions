67fbf8d4b26bd4b44ea8271fdece05c6ed8f4266
Utsav Shukla <utsav.academics@gmail.com>
2026-08-31 20:59:24 +0530

    [800] 2185A - 2026-08-31

--- codeforces/800/2185A_Perfect_Root.cpp
+++ codeforces/800/2185A_Perfect_Root.cpp
@@ -1,2 +1,17 @@
-# Problem: Perfect Root
-# URL: https://codeforces.com/contest/2185/submission/389014197
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--)
+    {
+        int x;
+        cin >> x;
+        for (int j = 1; j <= x; j++)
+        {
+            cout << j << " ";
+        }
+    }
+}
